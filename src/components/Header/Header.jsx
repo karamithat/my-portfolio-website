251903520a376e550bfcdb0a700977b790403853
karamithat <mithatkara93@gmail.com>
2025-06-29 15:31:43 +0300

    fix: correct image file extension for consistency

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CTA from "./CTA";
 import "./Header.css";
-import me from "../../assets/me.JPG";
+import me from "../../assets/me.jpg";
 import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
